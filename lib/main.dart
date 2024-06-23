6f670862cfe8ce8a7922918110e07c2ffdbb5ddb
Vitor <lukassgm@gmail.com>
2024-06-23 11:43:20 -0500

    adds project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,104 +8,165 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Algolia Helpers for Flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Create a multi searcher.
+  // The Searcher performs search requests and obtains search result
+  late MultiSearcher multiSearcher;
+  late FacetSearcher facetSearcher;
+  late HitsSearcher hitsSearcher;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _MyHomePageState() {
+    multiSearcher = MultiSearcher(
+      applicationID: 'latency',
+      apiKey: '1f6fd3a6fb973cb08419fe7d288fa4db',
+    );
+    hitsSearcher = multiSearcher.addHitsSearcher(
+      initialState: const SearchState(
+        indexName: 'instant_search',
+      ),
+    );
+    facetSearcher = multiSearcher.addFacetSearcher(
+        initialState: const FacetSearchState(
+      facet: 'brand',
+      searchState: SearchState(
+        indexName: 'instant_search',
+      ),
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Center(
+            child: TextField(
+              onChanged: (input) {
+                facetSearcher.query(input);
+                hitsSearcher.query(input);
+              }, // 3. Run your search operations
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search...',
+                border: InputBorder.none,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(children: [
+        const Header(title: 'Brands'),
+        Expanded(
+          child: StreamBuilder<FacetSearchResponse>(
+            // Listen and display facet search results
+            stream: facetSearcher.responses,
+            builder: (BuildContext context,
+                AsyncSnapshot<FacetSearchResponse> snapshot) {
+              if (snapshot.hasData) {
+                final response = snapshot.data;
+                final facets = response?.facetHits.toList() ?? [];
+                return ListView.builder(
+                  itemCount: facets.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final facet = facets[index];
+                    return ListTile(
+                      title: RichText(
+                        text: TextSpan(
+                          style: Theme.of(context).textTheme.titleSmall,
+                          children: facet.getHighlightedString().toInlineSpans()
+                            ..add(TextSpan(text: '(${facet.count})')),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+        const Divider(),
+        const Header(title: 'Items'),
+        Expanded(
+          child: StreamBuilder<SearchResponse>(
+            // Listen and display hits search results
+            stream: hitsSearcher.responses,
+            builder:
+                (BuildContext context, AsyncSnapshot<SearchResponse> snapshot) {
+              if (snapshot.hasData) {
+                final response = snapshot.data;
+                final hits = response?.hits.toList() ?? [];
+                return ListView.builder(
+                  itemCount: hits.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final hit = hits[index];
+                    return ListTile(
+                      title: RichText(
+                        text: TextSpan(
+                          style: Theme.of(context).textTheme.titleSmall,
+                          children:
+                              hit.getHighlightedString('name').toInlineSpans(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  final String title;
+
+  const Header({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: Text(
+          title,
+          textAlign: TextAlign.left,
+          style: const TextStyle(fontWeight: FontWeight.w800),
+        ),
+      ),
     );
   }
 }
